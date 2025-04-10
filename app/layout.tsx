d4feff7a68ad310d6f4d433a3e9ea00632cd4dc6
John-Salama <John.Salama.beshay@gmail.com>
2025-04-10 21:21:46 +0200

    feat: add Vercel Analytics integration for improved tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import AuthProvider from "@/app/components/AuthProvider";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
